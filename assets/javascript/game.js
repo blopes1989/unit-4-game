7cb3d9e17f643da9176b3273248bfde80a9e04e1
bizzy <blopes1989@gmail.com>
2018-05-28 00:09:57 -0400

    adding assets

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//variables
+var generated
+var one
+var two
+var three
+var four
+var win
+var loss
+var total
+
+
+//generate number
+
+
+//numbers for crystal
+
+
+//adding crystal together
+
+
+//declare winner loser start next round
+
+
+
+
+
