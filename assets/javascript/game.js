d3fc0aad147577b4905bafd522c5f1c94977dd8b
bizzy <blopes1989@gmail.com>
2018-05-30 23:55:21 -0400

    deadline

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,52 @@
+$(document).ready(function() {
 //variables
-var generated
-var one
-var two
-var three
-var four
-var win
-var loss
-var total
+var generated;
+var oneV;
+var twoV;
+var threeV;
+var fourV;
+var win = 0;
+var loss = 0;
+var myTotal = 0;
+var currentWins;
+var currentLosses;
 
 
 //generate number
-
+generated = Math.floor(Math.random() *120);
+$("#generate").text(generated);
 
 //numbers for crystal
+oneV = Math.floor(Math.random() *12)+1;
+
+twoV = Math.floor(Math.random() *12,)+1;
+
+threeV = Math.floor(Math.random() *12)+1;
+
+fourV = Math.floor(Math.random() *12)+1;
+
+$('button').click(function(){
+ myTotal = Number(myTotal) + Number($(this).val());
+ $('#total').text("myTotal: "+myTotal);      
+});
+$("#total").text(+myTotal)
+console.log(myTotal)
+//adding crystal together 
 
 
-//adding crystal together
+
+//compare to generated number
 
 
-//declare winner loser start next round
+//declare winner loser 
 
 
+//update win/loss
 
 
+//start next round
 
+
+$("generate").text(generated)
+
+});
