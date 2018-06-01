cde24fd44fdc3c2a46618cf553feeddb3526f332
bizzy <blopes1989@gmail.com>
2018-06-01 01:06:32 -0400

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,135 @@
-$(document).ready(function() {
-//variables
-var generated;
-var oneV;
-var twoV;
-var threeV;
-var fourV;
-var win = 0;
-var loss = 0;
-var myTotal = 0;
-var currentWins;
-var currentLosses;
+$(document).ready(function () {
+    //variables
+    var generated;
+    var oneV;
+    var twoV;
+    var threeV;
+    var fourV;
+    var win = 0;
+    var loss = 0;
+    var myTotal = 0;
+
+    //generate number
+    generated = Math.floor(Math.random() * 101 + 19);
+    $("#generate").text(generated);
+
+    //reset game
+    function reset() {
+        generated = Math.floor(Math.random() * 101 + 19);
+        $("#generate").text(generated);
+        myTotal = 0;
+        oneV = Math.floor(Math.random() * 12 + 1);
+
+        twoV = Math.floor(Math.random() * 12 + 1);
+
+        threeV = Math.floor(Math.random() * 12 + 1);
+
+        fourV = Math.floor(Math.random() * 12 + 1);
+    }
+
+    //numbers for crystal
+    oneV = Math.floor(Math.random() * 12 + 1);
+
+    twoV = Math.floor(Math.random() * 12 + 1);
+
+    threeV = Math.floor(Math.random() * 12 + 1);
+
+    fourV = Math.floor(Math.random() * 12 + 1);
 
 
-//generate number
-generated = Math.floor(Math.random() *101+19);
-$("#generate").text(generated);
+    //adding crystal together 
+    $('.crystals').on("click", ".one", function () {
+        myTotal = oneV + myTotal
+        $('#total').html(" Your total is now : " + myTotal);
+        console.log(myTotal)
 
-//numbers for crystal
-oneV = Math.floor(Math.random() *12+1);
+        if (myTotal == generated) {
+            win = win + 1
+            $('#wins').html(win);
+            reset();
 
-twoV = Math.floor(Math.random() *12+1);
+            alert("You Win!")
+        } else if (myTotal > generated) {
+            loss = loss + 1
+            $('#losses').html(loss);
+            alert("You Lose")
+            reset();
 
-threeV = Math.floor(Math.random() *12+1);
+        }
+    });
 
-fourV = Math.floor(Math.random() *12+1);
+    $('.crystals').on("click", ".two", function () {
+        myTotal = twoV + myTotal
+        $('#total').html(" Your total is now : " + myTotal);
+        console.log(myTotal)
+
+        if (myTotal == generated) {
+            win = win + 1
+            $('#wins').html(win);
+            reset();
+
+            alert("You Win!")
+        } else if (myTotal > generated) {
+            loss = loss + 1
+            $('#losses').html(loss);
+            alert("You Lose")
+            reset();
+        }
+    });
+    $('.crystals').on("click", ".three", function () {
+        myTotal = threeV + myTotal
+        $('#total').html(" Your total is now : " + myTotal);
+        console.log(myTotal)
+
+        if (myTotal == generated) {
+            win = win + 1
+            $('#wins').html(win);
+            reset();
+
+            alert("You Win!")
+        } else if (myTotal > generated) {
+            loss = loss + 1
+            $('#losses').html(loss);
+            alert("You Lose")
+            reset();
+        }
+    });
+
+    $('.crystals').on("click", ".four", function () {
+        myTotal = fourV + myTotal
+        $('#total').html(" Your total is now : " + myTotal);
+        console.log(myTotal)
+
+        if (myTotal == generated) {
+            win = win + 1
+            $('#wins').html(win);
+            reset();
+
+            alert("You Win!")
+        } else if (myTotal > generated) {
+            loss = loss + 1
+            $('#losses').html(loss);
+            alert("You Lose")
+            reset();
+        }
+    });
+
+    //compare to generated number
+    $("generate").text(generated)
 
 
-//adding crystal together 
-$('#one').click(function(){
-    myTotal =  oneV + myTotal
-    $('#total').text("myTotal: "+myTotal);  
-    console.log(myTotal)    
-});
-
-   $('#two').click(function(){
-    myTotal = (myTotal) + ($(twoV).val());
-    $('#total').text("myTotal: "+myTotal);      
-});
-
-   $('#three').click(function(){
-    myTotal = (myTotal) + ($(threeV).val());
-    $('#total').text("myTotal: "+myTotal);      
-});
-
-   $('#four').click(function(){
-    myTotal = (myTotal) + ($(fourV).val());
-    $('#total').text("myTotal: "+myTotal);
-    
-    
-    console.log(myTotal)
-});
-
-//compare to generated number
 
 
-//declare winner loser 
+    //declare winner loser 
 
 
-//update win/loss
 
 
-//start next round
 
 
-$("generate").text(generated)
+    //update win/loss
+
+
+    //start next round
 
 });
