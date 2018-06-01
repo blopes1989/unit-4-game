93ba2df58b026f16a1724fd0ed05ab29903787a8
bizzy <blopes1989@gmail.com>
2018-05-31 20:49:31 -0400

    new push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,27 +13,43 @@ var currentLosses;
 
 
 //generate number
-generated = Math.floor(Math.random() *120);
+generated = Math.floor(Math.random() *101+19);
 $("#generate").text(generated);
 
 //numbers for crystal
-oneV = Math.floor(Math.random() *12)+1;
+oneV = Math.floor(Math.random() *12+1);
 
-twoV = Math.floor(Math.random() *12,)+1;
+twoV = Math.floor(Math.random() *12+1);
 
-threeV = Math.floor(Math.random() *12)+1;
+threeV = Math.floor(Math.random() *12+1);
 
-fourV = Math.floor(Math.random() *12)+1;
+fourV = Math.floor(Math.random() *12+1);
 
-$('button').click(function(){
- myTotal = Number(myTotal) + Number($(this).val());
- $('#total').text("myTotal: "+myTotal);      
-});
-$("#total").text(+myTotal)
-console.log(myTotal)
+
 //adding crystal together 
+$('#one').click(function(){
+    myTotal =  oneV + myTotal
+    $('#total').text("myTotal: "+myTotal);  
+    console.log(myTotal)    
+});
 
+   $('#two').click(function(){
+    myTotal = (myTotal) + ($(twoV).val());
+    $('#total').text("myTotal: "+myTotal);      
+});
 
+   $('#three').click(function(){
+    myTotal = (myTotal) + ($(threeV).val());
+    $('#total').text("myTotal: "+myTotal);      
+});
+
+   $('#four').click(function(){
+    myTotal = (myTotal) + ($(fourV).val());
+    $('#total').text("myTotal: "+myTotal);
+    
+    
+    console.log(myTotal)
+});
 
 //compare to generated number
 
